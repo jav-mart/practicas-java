f47b822d137bcba41e6df55feb8808a315a9d512
Javier <rjbeltran@sertracen.com>
2024-10-21 17:50:41 -0600

    validadores

---
+++ poo_clases_abstractas/src/org/jbeltmart/pooclasesabstractas/form/validador/NoNulo.java
@@ -0,0 +1,19 @@
+package org.jbeltmart.pooclasesabstractas.form.validador;
+
+public class NoNulo extends Validador {
+    protected String mensaje = "el campo no puede ser nulo";
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return (valor != null);
+    }
+}
---
+++ poo_clases_abstractas/src/org/jbeltmart/pooclasesabstractas/form/validador/RequeridoValidador.java
@@ -0,0 +1,19 @@
+package org.jbeltmart.pooclasesabstractas.form.validador;
+
+public class RequeridoValidador extends Validador {
+    protected String mensaje = "el campo es requerido";
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return (valor != null && valor.length() > 0);
+    }
+}
